aa023094f656023e9ec14735458002ae166d101f
Roland Pangu <faruzzy@gmail.com>
2017-08-04 09:21:05 -0700

    Cleaner code

--- Java/Easy/ReverseFactorial/ReverseFactorial.java
+++ Java/Easy/ReverseFactorial/ReverseFactorial.java
@@ -8,27 +8,22 @@ import java.io.FileNotFoundException;
 
 public class ReverseFactorial {
 	private static void reverse(int value) {
-		int current = value;
-		int i = 2;
+		int number = value;
+		int divisor = 2;
 
-		while (current % i == 0) {
-			int result = current / i;
-			if (result != 1) {
-				i++;
-				current = result;
-			} else {
-				System.out.printf("%d = %d\n", value, current);
-				break;
-			}
+		while (number % divisor == 0) {
+			number /= divisor;
+			divisor++;
 		}
-		
-		System.out.println("None");
+
+		String output = (number == 1) ? String.format("%d = %d!\n", value, divisor -1) : "None"; 
+		System.out.print(output);
 	}
 
 	public static void main(String[] args) {
 		String inputFile = "input.in";
 		try {
-			String read = "";
+			String read;
 			BufferedReader in = new BufferedReader(new FileReader("input.in"));
 			while ((read = in.readLine()) != null) 
 				reverse(Integer.parseInt(read));
