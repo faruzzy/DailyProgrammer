2b647c664e17c2de03c4d2193f81eba261d02eec
Roland Pangu <faruzzy@gmail.com>
2017-11-02 10:54:47 -0700

    Small change

--- Java/Easy/Kaprekar/KaprekarNumber.java
+++ Java/Easy/Kaprekar/KaprekarNumber.java
@@ -1,7 +1,7 @@
 /*
  *  https://www.reddit.com/r/dailyprogrammer/comments/5aemnn/20161031_challenge_290_easy_kaprekar_numbers/
- *  In mathematics, a Kaprekar number for a given base is a non-negative integer, 
- *  the representation of whose square in that base can be split into two parts that add up to the original number again. 
+ *  In mathematics, a Kaprekar number for a given base is a non-negative integer,
+ *  the representation of whose square in that base can be split into two parts that add up to the original number again.
  *  For instance, 45 is a Kaprekar number, because 452 = 2025 and 20+25 = 45. The Kaprekar numbers are named after D. R. Kaprekar.
  *  I was introduced to this after the recent Kaprekar constant challenge.
  *  For the main challenge we'll only focus on base 10 numbers. For a bonus, see if you can make it work in arbitrary bases.
@@ -9,7 +9,7 @@
  * *****************************
  * Input Description
  * ****************************
- * Your program will receive two integers per line telling you the start and end of the range to scan, inclusively. 
+ * Your program will receive two integers per line telling you the start and end of the range to scan, inclusively.
  * Example:
  * 1 50
  *
@@ -77,7 +77,7 @@ public class KaprekarNumber {
 				}
 			}
 		}
-		
+
 		return sb.toString();
 	}
 }
